4d5ac70e4ea32918348dbbed0ead3f66f19d1217
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-04-12 21:31:22 -0600

    read json response

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const inviteKeyParam = urlParams.get("inviteKey");
 const form = document.getElementById("squiggle-form");
 const message = document.getElementById("message");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", async function (event) {
     event.preventDefault();
     const author = document.getElementById("author").value;
     const datetime = new Date().toISOString();
@@ -20,28 +20,24 @@ form.addEventListener("submit", function (event) {
     console.log("Request:");
     console.log(requestBody);
 
-    fetch("https://4ko9ppstm2.execute-api.us-west-2.amazonaws.com/prod/squiggle/", {
+    const response = await fetch("https://4ko9ppstm2.execute-api.us-west-2.amazonaws.com/prod/squiggle/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: requestBody,
-    })
-        .then((response) => {
-            if (response.ok) {
-                console.log("Success:");
-                console.log(response);
-                message.textContent = "Successfully submitted!";
-                message.style.color = "green";
-                form.reset();
-            } else {
-                throw new Error("Network response was not ok");
-            }
-        })
-        .catch((error) => {
-            console.log("Error:");
-            console.log(error);
-            message.textContent = "Error: " + error;
-            message.style.color = "red";
-        });
+    });
+
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log("Success:", jsonResponse);
+        message.textContent = "Successfully submitted!";
+        message.style.color = "green";
+        form.reset();
+    } else {
+        console.log("Error:");
+        console.log(response);
+        message.textContent = "Error";
+        message.style.color = "red";
+    }
 });
