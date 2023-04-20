d0e3414193b9b5138cda303ab58a6105cfe9e45a
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-04-19 21:59:38 -0600

    test svg POST method

--- js/p5_app.js
+++ js/p5_app.js
@@ -56,7 +56,7 @@ function draw() {
     drawSquiggle(squigglePoints, offScreenRenderer);
       // Get the SVG data as a string
     svgData = offScreenRenderer.elt.svg.outerHTML;
-    console.log(svgData);
+    // console.log(svgData);
 
     noLoop();
 }
@@ -258,3 +258,57 @@ class Point {
     }
 }
 
+
+
+// API stuff
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const inviteKeyParam = urlParams.get("inviteKey");
+// const form = document.getElementById("squiggle-form");
+// const message = document.getElementById("message");
+
+// form.addEventListener("submit", async function (event) {
+async function sendData(inviteKey) {
+    // event.preventDefault();
+    // const author = document.getElementById("author").value;
+    const author = "Deanna";
+    const datetime = new Date().toISOString();
+
+    const request = {
+        inviteKey: inviteKey,// inviteKeyParam,
+        squiggle: {
+            datetime,
+            author,
+            svgData,
+        },
+    };
+    const requestBody = JSON.stringify(request)
+
+    console.log("Request:");
+    console.log(requestBody);
+
+    const response = await fetch("https://4ko9ppstm2.execute-api.us-west-2.amazonaws.com/prod/squiggle/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: requestBody,
+    });
+
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        const responseBody = JSON.parse(jsonResponse.body);
+        console.log("Success:", responseBody.message);
+
+        // message.textContent = "Successfully submitted!";
+        // message.style.color = "green";
+        // form.reset();
+    } else {
+        const jsonResponse = await response.json();
+        const responseBody = JSON.parse(jsonResponse.body);
+        console.log("Error:", responseBody.message);
+        
+        // message.textContent = "Error";
+        // message.style.color = "red";
+    }
+};
