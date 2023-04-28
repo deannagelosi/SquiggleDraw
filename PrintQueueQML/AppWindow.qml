50ae6c467580ea41527b9831918c6af60032609b
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-04-28 17:11:26 -0600

    initial touchscreen UI

---
+++ PrintQueueQML/AppWindow.qml
@@ -0,0 +1,79 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+
+ApplicationWindow {
+    visible: true
+    width: 800
+    height: 480
+    title: "SquiggleQueue"
+
+    Text {
+        id: title
+        width: 232
+        height: 54
+        color: "#000000"
+        text: qsTr("SquiggleDraw")
+        font.pixelSize: 32
+        horizontalAlignment: Text.AlignHCenter
+        verticalAlignment: Text.AlignVCenter
+        wrapMode: Text.Wrap
+        font.weight: Font.Normal
+        anchors.verticalCenterOffset: -191
+        font.family: "IBM Plex Sans"
+        anchors.horizontalCenterOffset: 1
+        anchors.centerIn: parent
+    }
+
+    Rectangle {
+        id: controls_box
+        x: 631
+        y: 94
+        width: 134
+        height: 352
+        color: "#f0f0f0"
+        radius: 6
+        border.color: "#40000000"
+        border.width: 1
+    }
+
+    Play {
+        id: play_button
+        objectName: "play_button"
+        x: 648
+        y: 140
+        width: 100
+        height: 100
+    }
+
+    Stop {
+        id: stop_button
+        objectName: "stop_button"
+        x: 648
+        y: 306
+        width: 100
+        height: 100
+    }
+
+    Rectangle {
+        id: table_placeholder
+        x: 30
+        y: 94
+        width: 573
+        height: 352
+        color: "#f0f0f0"
+        radius: 6
+        border.color: "#40000000"
+        border.width: 1
+    }
+
+
+}
+
+/*##^##
+Designer {
+    D{i:0;uuid:"0fd394c3-a284-58b9-aca1-30faf0e7fc1b"}D{i:1;uuid:"18210b84-7fe3-5a35-8b4f-adc5ff295785"}
+D{i:2;uuid:"eb13b562-0d10-5457-ba05-f0384de30a0c"}D{i:3;uuid:"ba79c8c6-d40f-5f54-8228-39b86d6677f4_instance"}
+D{i:4;uuid:"4c6f14a1-152d-5dd7-a9bc-4e515bc123e4"}D{i:5;uuid:"a68b2aaa-50f0-5eb6-a71c-7e294f1cd485_instance"}
+}
+##^##*/
+
---
+++ touchscreen_UI.py
@@ -0,0 +1,55 @@
+import sys, math
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtCore import QTimer, QObject, pyqtSlot, pyqtSignal
+
+def main(): 
+    # Launch the UI
+    app = QApplication(sys.argv)
+    # Create an instance of the controller and add it to the QML context
+    controller = Controller();
+    # Execute application and wait for exit
+    sys.exit(app.exec())
+
+class Controller(QObject):
+    def __init__(self):
+        super().__init__()
+        # Create and initialize the QML engine
+        self.engine = QQmlApplicationEngine()
+        self.engine.load('PrintQueueQML/AppWindow.qml')
+        self.root_object = self.engine.rootObjects()[0]
+        self.setup_screen()
+
+        # Setup global program variables
+
+
+        # Fetch DB data and update
+
+        # Create a QTimer to update every x seconds
+        # self.timer = QTimer()
+        # self.timer.timeout.connect(self.updateValues)
+        # self.timer.start(seconds_update * 1000)
+
+    def setup_screen(self):
+        # Two kinds of configuration: Listeners and Properties
+        # Listeners let us know when a element is clicked or value updated (qt calls them slots)
+        # Properties control the appearance of an element with .setProperty("property", "value")
+
+        # Setup buttons
+        stop_button = self.get_object("stop_button")
+        stop_button.setProperty("state", "state_unavailable")
+
+        play_button = self.get_object("play_button")
+        play_button.setProperty("state", "state_ready")
+
+    def get_object(self, object_name):
+        # Search for specific components by objectName
+        obj = self.root_object.findChild(QObject, object_name)
+
+        if obj is None: 
+            raise ValueError(f"Can't find object: {object_name}")
+        
+        return obj
+    
+if __name__ == '__main__':
+    main()
